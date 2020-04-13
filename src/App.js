500d39b84625e8b929a20d4e06f10493169f7187
gabiristea <risteagabi@gmail.com>
2020-04-13 14:23:29 +0300

    change app text

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Distractie folosind git branch :)
         </a>
       </header>
     </div>
